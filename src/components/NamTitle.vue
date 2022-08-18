cd789b84c008b28202290ce7dbf47a97ce62d032
Cauchyzor <cauchyzor@gmail.com>
2022-08-18 18:23:48 +0200

    Create component for page Title

---
+++ src/components/NamTitle.vue
@@ -0,0 +1,25 @@
+<template>
+    <ion-text>
+        <h5 class="page-title">
+            <slot></slot>
+        </h5>
+    </ion-text>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import { IonText } from "@ionic/vue";
+
+export default defineComponent({
+    name: "NamTitle",
+    components: {
+        IonText
+    }
+});
+</script>
+<style scoped>
+.page-title {
+    text-transform: Uppercase;
+    font-family: "Anurati-Regular";
+}
+</style>
