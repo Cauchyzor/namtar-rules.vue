81d20362d6d3416af9129d8160c022efabaf3029
Cauchyzor <cauchyzor@gmail.com>
2022-10-05 18:09:23 +0200

    Init equipement item

---
+++ src/components/EquipementItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <ion-item>
+    <ion-label class="ion-text-wrap">
+      <p>{{ EquipementItem.Nom }}</p>
+      <!-- TODO : Ajouter Description de la capacitée et travailler un look concis -->
+    </ion-label>
+    <ion-button fill="outline" slot="end">Détail</ion-button>
+  </ion-item>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { PropType } from "vue";
+
+import { IonItem, IonLabel, IonButton } from "@ionic/vue";
+
+import { Equipement } from "@/domain/Equipement";
+
+export default defineComponent({
+  components: {
+    IonItem,
+    IonLabel,
+    IonButton,
+  },
+  props: {
+    EquipementItem: { type: Object as PropType<Equipement>, required: true },
+  },
+});
+</script>
+<style scoped></style>
