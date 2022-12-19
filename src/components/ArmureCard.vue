c87261ea34564ababc212c330908d923ab7a2cc7
pierrot.cauchy <pierrot.cauchy@aifrance.com>
2022-12-19 14:41:52 +0100

    Create armure model and armur item

---
+++ src/components/ArmureCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-card>
+    <q-card-section vertical>
+      <div class="text-overline">
+        Encombrement : {{ Armure.Encombrement }} - Prix :
+        {{ Armure.Prix }}
+      </div>
+      <div class="text-h5 q-mt-sm q-mb-xs">
+        {{ Armure.Nom }}
+      </div>
+      <div class="text-caption text-grey">{{ Armure.Description }}</div>
+    </q-card-section>
+  </q-card>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+import { ArmureItem } from 'src/domain/Armures';
+
+export default defineComponent({
+  name: 'EquipementCard',
+
+  props: {
+    Armure: { type: Object as PropType<ArmureItem>, required: true },
+  },
+});
+</script>
+<style scoped></style>
--- src/domain/Armures.ts
+++ src/domain/Armures.ts
@@ -4,3 +4,41 @@ export interface ArmureItem extends EquipementItem {
   Defense: number;
   Special: Array<string>;
 }
+
+export class ArmureService {
+  private static EquipementItems: Array<ArmureItem> = [
+    {
+      Nom: 'Ceinture multi-usage',
+      Encombrement: 1,
+      Rarete: 1,
+      Description: "Permet d'éclairer une large zone autour du porteur",
+      Prix: 50,
+      Defense: 0,
+      Special: [],
+    },
+    {
+      Nom: 'Cartouchière',
+      Encombrement: 1,
+      Rarete: 1,
+      Description:
+        "Accès rapides sur des celulles ou munitions jusquà 8 d'encombrement",
+      Prix: 50,
+      Defense: 0,
+      Special: [],
+    },
+    {
+      Nom: 'Armures legères',
+      Encombrement: 1,
+      Rarete: 1,
+      Description:
+        'Ensemble avec une veste et un pantalon renforcé avec des plaques de metal. Sommaire et bon marché',
+      Prix: 50,
+      Defense: 1,
+      Special: [],
+    },
+  ];
+
+  static getAllEquipementItems() {
+    return this.EquipementItems;
+  }
+}
