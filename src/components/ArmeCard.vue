310935d2c41c28c2723cfe5e5dea859162e181ba
pierrot.cauchy <pierrot.cauchy@aifrance.com>
2022-12-27 17:18:07 +0100

    Improve ArmeCard

--- src/components/ArmeCard.vue
+++ src/components/ArmeCard.vue
@@ -6,9 +6,18 @@
         {{ Arme.Prix }}
       </div>
       <div class="text-h5 q-mt-sm q-mb-xs">
-        {{ Arme.Nom }}
+        {{ Arme.Nom }} ({{ Arme.Competence }})
+      </div>
+      <div class="text-overline">
+        {{ Arme.Degats }}
       </div>
       <div class="text-caption text-grey">{{ Arme.Description }}</div>
+      <div
+        v-show="Arme.Special.length > 0"
+        class="q-mt-md text-caption text-grey"
+      >
+        Spécial : {{ Arme.Special.join(' - ') }}
+      </div>
     </q-card-section>
   </q-card>
 </template>
