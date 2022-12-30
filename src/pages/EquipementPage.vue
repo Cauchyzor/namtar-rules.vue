376262eb772cb2b91fe2a4403f39f1d9ecbbf0cc
pierrot.cauchy <pierrot.cauchy@aifrance.com>
2022-12-30 14:13:22 +0100

    Add rarity concept to rule

--- src/pages/EquipementPage.vue
+++ src/pages/EquipementPage.vue
@@ -11,6 +11,19 @@
       valeurs de 0 ( ce qui ne signifie pas pour autant que l&#39;ont peut en
       prendre autant que voulut, c&#39;est au MJ de doser ).
     </p>
+    <h5>Rareté et prix de base</h5>
+    <p>
+      Chaque objet possède une valeur de rareté qui augmente avec la difficulté
+      de trouver un tel objet pour un personnage. Lorsqu'un personnage souhaite
+      obtenir un objet, il lance un test de négociation dont la difficulté
+      dépend de la rareté. Si le test réussi, le nombre de succès peux suggérer
+      une bonne affaire en réduisant par exemple le prix auquel l'objet est
+      vendu par rapport au prix de base. Si le test génère des avantages, cela
+      peut signifier qu'un objet d'habitude en quantité très réduite sera trouvé
+      en plusieurs exemplaires. Les désavantages peuvent à l'inverse venir
+      gonfler le prix de l'objet bar rapport au prix de base, ou indiquer que la
+      recherche de l'objet prends plus de temps que prévu.
+    </p>
     <h4>Équipement quotidien</h4>
 
     <div class="row q-col-gutter-sm justify-center">
@@ -73,8 +86,6 @@
         <ArmureCard :key="armure.Nom" :Armure="armure"></ArmureCard>
       </div>
     </div>
-    <h4>Véhicules et montures</h4>
-    <p>//TODO</p>
   </q-page>
 </template>
 
