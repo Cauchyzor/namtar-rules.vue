cfad045fcd249eb4fe7f7b4a75ee657201802119
Cauchyzor <cauchyzor@gmail.com>
2024-04-06 17:19:50 +0200

    Modification du model Creature

--- src/model/Creature.ts
+++ src/model/Creature.ts
@@ -1,7 +1,8 @@
+import { Aptitude } from "./Aptitude";
+import { Arme } from "./Armes";
 import { AttributsName } from "./Attribut";
 import { CaracteritiqueName } from "./Caracteristique";
 import { CompetenceName } from "./Competence";
-import { Genotrait } from "./Genotrait";
 
 export class Creature {
   /**
@@ -13,13 +14,9 @@ export class Creature {
    */
   NiveauEffectif: number;
   /**
-   * Liste de Caracteristiques avec leur valeurs
+   * Liste de Caractéristiques avec leur valeurs
    */
   Caracteristiques: Map<CaracteritiqueName, number>;
-  /**
-   * Liste des attribut génétiques qui modifieront les attributs
-   */
-  Genotraits: Map<Genotrait, number>;
   /**
    * Listes des valeurs pour toutes les compétences accessible par la créatures
    */
@@ -27,100 +24,43 @@ export class Creature {
   /**
    * Listes des valeurs pour toutes les compétences accessible par la créatures
    */
-  Aptitudes: Set<string>;
+  Attributs: Map<AttributsName, string>;
+  /**
+   * Listes des valeurs pour toutes les compétences accessible par la créatures
+   */
+  Aptitudes: Array<Aptitude>;
+  /**
+   * Listes des valeurs pour toutes les compétences accessible par la créatures
+   */
+  Armes: Array<Arme>;
 
-  constructor() {
-    this.Nom = "Creature inconnue";
-    this.NiveauEffectif = 1;
-    this.Caracteristiques = new Map([
+  constructor(
+    nom = "Creature inconnue",
+    niveau = 1,
+    caracteristique = new Map([
       [CaracteritiqueName.VIGUEUR, 0],
       [CaracteritiqueName.AGILITE, 0],
       [CaracteritiqueName.ADRESSE, 0],
       [CaracteritiqueName.INTELLIGENCE, 0],
       [CaracteritiqueName.CHARISME, 0],
-    ]);
-    this.Competences = new Map([]);
-    this.Genotraits = new Map([]);
-    this.Aptitudes = new Set([]);
-  }
-
-  /**
-   * Retourne le nombre de PV max de la creature
-   *
-   * @returns PVmax
-   */
-  computePoolPV(): string {
-    return `${
-      (this.Caracteristiques.get(CaracteritiqueName.VIGUEUR) || 0) +
-      4 +
-      Array.from(this.Genotraits.entries())
-        .map(([g, rank]) => (g.Modificateurs.get(AttributsName.PV) || 0) * rank)
-        .reduce((a, b) => a + b, 0) // accumulator
-    } + ${this.NiveauEffectif}d4`;
-  }
-  /**
-   * Retourne le nombre de point de résilience
-   *
-   * @returns point de Résilience max
-   */
-  computePoolRecuperation(): number {
-    return (
-      (this.Caracteristiques.get(CaracteritiqueName.VIGUEUR) || 0) +
-      Array.from(this.Genotraits.entries())
-        .map(
-          ([g, rank]) =>
-            (g.Modificateurs.get(AttributsName.RECUPERATION) || 0) * rank
-        )
-        .reduce((a, b) => a + b, 0) // accumulator
-    );
-  }
-  /**
-   * Retourne le score max de réflexes de la creature
-   *
-   * @returns
-   */
-  computePoolReflexes(): number {
-    return (
-      (this.Caracteristiques.get(CaracteritiqueName.AGILITE) || 0) +
-      Array.from(this.Genotraits.entries())
-        .map(
-          ([g, rank]) =>
-            (g.Modificateurs.get(AttributsName.REFLEXES) || 0) * rank
-        )
-        .reduce((a, b) => a + b, 0) // accumulator
-    );
-  }
-  /**
-   * Retourne le score max d'initiative de la creature
-   *
-   * @returns
-   */
-  computePoolInitiative(): number {
-    return (
-      (this.Caracteristiques.get(CaracteritiqueName.AGILITE) || 0) +
-      Array.from(this.Genotraits.entries())
-        .map(
-          ([g, rank]) =>
-            (g.Modificateurs.get(AttributsName.INITIATIVE) || 0) * rank
-        )
-        .reduce((a, b) => a + b, 0) // accumulator
-    );
-  }
-
-  /**
-   * Retourne le maximum du pool de stress de la creature
-   *
-   * @returns
-   */
-  computePoolStress(): number {
-    return (
-      (this.Caracteristiques.get(CaracteritiqueName.INTELLIGENCE) || 0) +
-      (this.Caracteristiques.get(CaracteritiqueName.CHARISME) || 0) +
-      Array.from(this.Genotraits.entries())
-        .map(
-          ([g, rank]) => (g.Modificateurs.get(AttributsName.STRESS) || 0) * rank
-        )
-        .reduce((a, b) => a + b, 0) // accumulator
-    );
+    ]),
+    attributs = new Map([
+      [AttributsName.INITIATIVE, "0"],
+      [AttributsName.PV, "1+1d4"],
+      [AttributsName.RECUPERATION, "0"],
+      [AttributsName.REFLEXES, "0"],
+      [AttributsName.STRESS, "0"],
+    ]),
+    competences: Map<CompetenceName, number> = new Map([]),
+    aptitudes: Array<Aptitude> = [],
+    armes: Array<Arme> = []
+  ) {
+    this.Nom = nom;
+    this.NiveauEffectif = niveau;
+    this.Caracteristiques = caracteristique;
+    this.Competences = competences;
+    this.Attributs = attributs;
+    this.Aptitudes = aptitudes;
+    this.Armes = armes;
   }
 }
