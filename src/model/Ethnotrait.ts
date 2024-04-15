0e57cc811ec434682dd0d0bc45a875f6221bcb37
pierrot.cauchy <pierrot.cauchy@aifrance.com>
2024-04-15 17:23:02 +0200

    Avantage et désavantage VS Supériorité et handicap

--- src/model/Ethnotrait.ts
+++ src/model/Ethnotrait.ts
@@ -14,14 +14,14 @@ export class EthnotraitService {
     {
       Nom: "Massif",
       Description:
-        "Votre valeur de vigueur minimal est de 3. Vous avez 5 PV supplémentaire et beneficiez d'un avantage sur vos empoignades et vos bousculade.",
+        "Votre valeur de vigueur minimal est de 3. Vous avez 5 PV supplémentaire et bénéficiez d'un dé de supériorité sur vos empoignades et vos bousculade.",
       CoherenceGénétiques: 2,
       Modificateurs: new Map([]),
     },
     {
       Nom: "Maladroit",
       Description:
-        "Vous avez un désavantage sur tout vos test de coordination.",
+        "Vous subissez un dé d'handicap sur tout vos test de coordination.",
       CoherenceGénétiques: -1,
       Modificateurs: new Map([]),
     },
@@ -42,21 +42,21 @@ export class EthnotraitService {
     {
       Nom: "Producteur de biomatériaux",
       Description:
-        "Vous pouvez générer à la demande un materiaux liquide organique au prix de vos PV (1l contre 20% de vos PV maximal). Les propriétées du materiaux peuvent être déterminée par d'autre ethnotraits",
+        "Vous pouvez générer à la demande un matériaux liquide organique au prix de vos PV (1l contre 20% de vos PV maximal). Les propriétés du matériaux peuvent être déterminée par d'autre ethnotraits",
       CoherenceGénétiques: 1,
       Modificateurs: new Map([]),
     },
     {
       Nom: "Inflammable",
       Description:
-        "Votre sang et les biomateraux que vous produisez sont inflammable",
+        "Votre sang et les biomatériaux que vous produisez sont inflammable",
       CoherenceGénétiques: 1,
       Modificateurs: new Map([]),
     },
     {
       Nom: "Organes vocaux primitif",
       Description:
-        "Il est difficile pour vous de produire des son necessairFe aux language. Vous ne pouvez vous exprimer que dans votre langue naturelle ou augmenter de 2 le DD des tests qui impliquent la parole comme composante principale pour sa réussite.",
+        "Il est difficile pour vous de produire des son nécessaire aux language. Vous ne pouvez vous exprimer que dans votre langue naturelle ou augmenter de 2 le DD des tests qui impliquent la parole comme composante principale pour sa réussite.",
       CoherenceGénétiques: -1,
       Modificateurs: new Map([]),
     },
