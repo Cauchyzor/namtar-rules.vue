1c8403fd5e054f69bdcad57b74cc45476e37613b
Cauchyzor <cauchyzor@gmail.com>
2024-05-18 12:07:25 +0200

    Apt database: ajout de filtre par types

--- src/pages/PageAptitudesDatabase.vue
+++ src/pages/PageAptitudesDatabase.vue
@@ -1,9 +1,20 @@
 <template>
   <q-page padding>
     <h2 class="text-center">Database : Aptitudes</h2>
+    <div class="q-pa-md">
+      <q-option-group
+        name="Types d'Aptitude"
+        v-model="FilterByType"
+        :options="AptTypes"
+        type="checkbox"
+        color="primary"
+        class="bg-secondary"
+        inline
+      ></q-option-group>
+    </div>
     <div class="row q-col-gutter-sm justify-center items-stretch">
       <div
-        v-for="Aptitude in AptitudesList"
+        v-for="Aptitude in filteredApitudesList"
         :key="Aptitude.Nom"
         class="col-12 col-sm-4"
       >
@@ -17,7 +28,7 @@ import { defineComponent } from "vue";
 
 import AptitudeCard from "src/components/AptitudeCard.vue";
 
-import { ServiceAptitude } from "src/data/ServiceAptitude";
+import { ServiceAptitude, AptitudeTypeName } from "src/data/ServiceAptitude";
 
 export default defineComponent({
   name: "PersonnagePage",
@@ -26,8 +37,21 @@ export default defineComponent({
   },
   data() {
     return {
+      AptTypes: Object.values(AptitudeTypeName).map((t) => {
+        return { label: t, value: t };
+      }),
       AptitudesList: ServiceAptitude.findAllAptitudes(),
+      FilterByType: new Array<AptitudeTypeName>(),
     };
+  },
+  computed: {
+    filteredApitudesList() {
+      return this.FilterByType.length > 0
+        ? this.AptitudesList.filter((apt) =>
+            this.FilterByType.includes(apt.Type.Nom)
+          )
+        : this.AptitudesList;
+    },
   },
 });
 </script>
