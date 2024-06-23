549708366e08719755d2cb07a4b84cbca3165a41
Cauchyzor <cauchyzor@gmail.com>
2024-06-23 15:10:03 +0200

    Update lore

--- src/pages/PageLoreIntroduction.vue
+++ src/pages/PageLoreIntroduction.vue
@@ -5,51 +5,45 @@
   <!--TODO : Introduire le concept d'"effacement des distance" avec le fluide -->
   <!--TODO : Trouver un autre wording pour ethernum -->
   <q-page padding class="row items-center justify-evenly">
-    <h2>Introduction</h2>
-    <p class="text-center">
+    <h2>Histoire commune</h2>
+    <p class="text-justify">
       L'éther est le liant. Dans l'infinité de l'univers, il est le mystérieux
       intermédiaire qui permet à toute chose d'être à la fois immensément loin
       et, pourtant, presque au point de se toucher...
     </p>
-
-    <p>
+    <h3>Avant le Namtar</h3>
+    <p class="text-justify">
       La galaxie est en constante ébullition ; des civilisations naissent et
       périssent, des guerres se déclarent, et se terminent… Et tout le monde
       s'en fout en fait. On se dit que c'est dans la nature des êtres vivants
-      doués de conscience. Les grandes failles d'ethernum apparaissent
+      doués de conscience. Les grandes failles dimensionnelles apparaissent
       spontanément, offrant la possibilité aux nouveaux voisins jusqu'alors
       isolés de grandir ensemble ou de s'annihiler. Avec une telle effervescence
       du monde, chacun essaie de trouver sa voie en jouant des coudes.
     </p>
-    <p>
-      Seulement, voilà, ça avait beau être déjà un joyeux bordel, il fallait que
-      quelque chose vienne ajouter sa pierre à l'édifice, comme pour prouver
-      qu'on peut toujours en faire plus… C'est parti de rien, en plus. Des
-      fanatiques de l'exploration galactique, conscients que les 10^22 systèmes
-      grouillant dans la galaxie ne demandaient qu'à être explorés, et
-      prospectés (pillés), ont profiter de certaines failles temporaires menant
-      au centre de la galaxie. Ici, ils découvrirent un monde impossible,
-      indescriptible par les outils que la science s'évertuait à établir. Mieux
-      encore, en plein cœur de ce chaos se trouvait des zones habitables (oui,
-      je vous le jure.). Et qui fut habité. Des vestiges d'une grande culture,
-      si on se réfère aux architectures grandioses et aux objets finement
-      ouvragés retrouvés lors des excavations de la planète par les
-      explorateurs. Cette civilisation n'avait sans doute pas encore découvert
-      la poudre à canon, et pourtant, des structures immenses à l'architecture
-      avec des angles impossibles se dressaient vers le ciel. Rapidement, les
-      colifichets et les artefacts de cette civilisation se rependirent à
-      travers toutes les galaxies. Synonyme de richesse, de pouvoir, de culture,
-      ils attiraient la curiosité du contemporain, du monarque, de l'homme de
-      science, comme de l'historien avide de percer le secret de la
-      civilisation. Les excavations des mondes impossibles continuèrent
-      d'avantage. Chaque planète étant une incroyable mille-feuille de
-      curiosités. Du candélabre à la masse d'armes, en passant par des
-      instruments sortis d'une époque moyenâgeuse, tout était vendu à un prix
-      démesuré. Mais ils creusèrent trop profondément, et avec trop d'avidité…
-      Et dans les ténèbres, ils le réveillèrent.
+    <h4>Une anomalie terrifiante</h4>
+    <p class="text-justify">
+      Des fanatiques de l'exploration galactique, conscients que les 10^22
+      systèmes grouillant dans la galaxie ne demandaient qu'à être explorés et
+      prospectés (pillés), ont profité de certaines failles menant au centre de
+      la galaxie. Ici, ils découvrirent un monde impossible, impermeable aux
+      outils que la science s'évertuait à établir. Des vestiges d'une grandes
+      culture aux architectures grandioses et aux objets finement ouvragés.
+      Cette civilisation n'avait sans doute pas encore découvert la poudre à
+      canon, et pourtant, des structures immenses à l'architecture avec des
+      angles impossibles se dressaient vers le ciel. Rapidement, les colifichets
+      et les artefacts de cette civilisation se rependirent à travers toutes les
+      galaxies. Synonyme de richesse, de pouvoir, de culture, ils attiraient la
+      curiosité du contemporain, du monarque, de l'homme de science, comme de
+      l'historien avide de percer le secret de la civilisation. Les excavations
+      des mondes impossibles continuèrent d'avantage. Chaque planète étant une
+      incroyable mille-feuille de curiosités. Du candélabre à la masse d'armes,
+      en passant par des instruments sortis d'une époque moyenâgeuse, tout était
+      vendu à un prix démesuré. Mais ils creusèrent trop profondément, et avec
+      trop d'avidité… Et dans les ténèbres, ils le réveillèrent.
     </p>
-    <p>Le Namtar...</p>
-    <p>
+    <h4>L'incident</h4>
+    <p class="text-justify">
       Une anomalie de la biologie, comme de l'esprit. Un monstre titanesque,
       inimaginable, aspirant insatiablement la vie et l'énergie des créatures
       dans sa proximité. Dans l'horreur et la panique, les explorateurs de
@@ -62,22 +56,23 @@
       les plus grandes de la galaxie. Dans un big-bang apocalyptique, les
       planètes les plus proches du point d'impact furent balayées. La détonation
       fit changer d'orbite plusieurs étoiles sur des années-lumières de
-      diamètre, et les failles d'etherum, celles qui permirent aux explorateurs
-      l'accès au cœur, s'effondrèrent, isolant alors les forces en présence du
-      reste de la galaxie. Le contact fut perdu.
+      diamètre, et les failles dimensionnelles, celles qui permirent aux
+      explorateurs l'accès au cœur, s'effondrèrent, isolant alors les forces en
+      présence du reste de la galaxie. Le contact fut perdu.
     </p>
-    <p>
+    <h4>Après l’incident</h4>
+    <p class="text-justify">
       Mais l'histoire ne s'arrêta pas là. Une énergie nouvelle, une sorte de
-      fluide, s'éparpilla dans toute la galaxie. Peu de temps après, des
-      personnes avec d'étranges pouvoirs firent leurs apparitions aux quatre
-      coins de la galaxie, prétendant qu'il pouvait manipuler cette énergie
-      malléable pour accomplir de grandes choses. Les possesseurs d'artefacts et
-      d'objets originaires du cœur remarquèrent alors qu'une force nouvelle
-      habitait également leurs inoffensifs ustensiles décoratifs. Des pouvoirs
-      au delà de la raison, des capacités hors normes. Une nouvelle chasse à
-      l'artefact était donnée.
+      fluide, s'éparpilla dans toute la Des creatures et des personnes avec
+      d'étranges pouvoirs firent leurs apparitions aux quatre coins de la
+      galaxie, prétendant qu'il pouvait manipuler cette énergie malléable pour
+      accomplir de grandes choses. Les possesseurs d'artefacts et d'objets
+      originaires du cœur remarquèrent alors qu'une force nouvelle habitait
+      également leurs inoffensifs ustensiles décoratifs. Des pouvoirs au delà de
+      la raison, des capacités hors normes. Une nouvelle chasse à l'artefact
+      était donnée.
     </p>
-    <p class="text-center">Ainsi débuta l'ère du fluide...</p>
+    <p class="text-justify">Ainsi débuta l'ère du fluide...</p>
   </q-page>
 </template>
 <script lang="ts">
